1d758fc0c33fb63ee78dd0746d06f7cfdeb6f086
Dennis Wagelaar <dwagelaar@gmail.com>
2018-04-24 22:12:20 +0200

    Initial contents

---
+++ src/main/java/be/denniswagelaar/emftvm/maven/example/App.java
@@ -0,0 +1,13 @@
+package be.denniswagelaar.emftvm.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/be/denniswagelaar/emftvm/maven/example/AppTest.java
@@ -0,0 +1,19 @@
+package be.denniswagelaar.emftvm.maven.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+	@Test
+	public void testApp()
+	{
+		assertTrue(true);
+	}
+
+}
